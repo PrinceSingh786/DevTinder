3ca0cfd142717fbacef08fd02591258b2de77691
Prince Singh <psjeee8@gmail.com>
2025-05-23 10:52:08 +0530

    postman

--- src/app.js
+++ src/app.js
@@ -2,12 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello World from server using Express bbhaiya!");
+// Route handlers are given below
+
+// This route will match any request to the root URL ("/test") and respond with "Hello World!"
+// app.use("/test", (req, res) => {
+//   res.send("Hello World from server using Express bbhaiya!");
+// });
+
+// app.use("/jai", (req, res) => {
+//   res.send("Jai jai  jai ho! from server using Express!");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Ram Ram  bhaiya ji");
+// });
+
+// This will match only GET requests to the "/user" URL
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Prince", lastname: "Singh" });
 });
 
-app.use("/jai", (req, res) => {
-  res.send("Jai jai  jai ho! from server using Express!");
+app.post("/us er", (req, res) => {
+  //saving data to database
+  res.send("Data saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  //deleting data from database
+  res.send("Data deleted from database");
 });
 
 app.listen(3333, () => {
