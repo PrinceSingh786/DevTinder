e5518a79e43adf8cc82f5a66d190c15646cf6c18
Prince Singh <psjeee8@gmail.com>
2025-05-26 18:13:46 +0530

    api data sanitization

--- src/app.js
+++ src/app.js
@@ -13,23 +13,57 @@ const app = express();
 //   res.send("Jai jai  jai ho! from server using Express!");
 // });
 
-// app.use("/", (req, res) => {
-//   res.send("Ram Ram  bhaiya ji");
-// });
+// app.use("/user", [
+//   (req, res, next) => {
+//     console.log("Request received at /user");
+//     next();
+//     console.log("first hu");
+//     // res.send("Ram   bhaiya ji");
+//   },
+//   (req, res, next) => {
+//     console.log("2nd hu");
+//     next();
+//     // res.send("Ram Ram  bhaiya ji");
+//   },
+//   (req, res) => {
+//     console.log("3rd hu");
+//     res.send("Ram Rm  bhaiya ji");
+//   },
+// ]);
 
 // This will match only GET requests to the "/user" URL
+// app.get("/user/:userID/:age", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstname: "Prince", lastname: "Singh" });
+// });
+
+// app.post("/user", (req, res) => {
+//   //saving data to database
+//   res.send("Data saved to database");
+// });
+
+// app.delete("/user", (req, res) => {
+//   //deleting data from database
+//   res.send("Data deleted from database");
+// });
+
+const { auth } = require("./Middlewares/auth");
+app.use("/admin", auth);
+
+app.get("/dashboard", (req, res) => {
+  try {
+    throw new Error("This is an error from the dashboard route");
+    res.send("Welcome to the admin ");
+  } catch (err) {
+    console.error("Error occurred:", err);
+    res
+      .status(500)
+      .send("Prince Ji An error occurred while processing your request.");
+  }
+  // res.send("Welcome to the admin dashboard");
+});
 app.get("/user", (req, res) => {
-  res.send({ firstname: "Prince", lastname: "Singh" });
-});
-
-app.post("/us er", (req, res) => {
-  //saving data to database
-  res.send("Data saved to database");
-});
-
-app.delete("/user", (req, res) => {
-  //deleting data from database
-  res.send("Data deleted from database");
+  res.send("Welcome to the user ");
 });
 
 app.listen(3333, () => {
